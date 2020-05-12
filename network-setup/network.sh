faa3f5e101bfde928cecd900a740ea9b6b7af25d
valadaa48 <valadaa48@gmx.com>
2020-05-12 03:41:23 +0000

    Update network-setup

--- network-setup/network.sh
+++ network-setup/network.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-#/usr/bin/nmcli dev wifi connect ssid password passphrase
+# Change ssid and passphrase and uncomment the line
+# When your network is up, comment the line again
+
+# NOTE: You can repeat this command to add multiple connections
+#       or run this command at a later time
+
+#/usr/bin/nmcli dev wifi connect "ssid" password "passphrase"
 
