50d92fdd3614af418c9dea3cc816aac2f293444c
npaladin2000 <43549566+npaladin2000@users.noreply.github.com>
2020-05-27 10:58:53 -0400

    Update firstboot2.sh
    Add automatic creation of ROM folders defined in es_systems.cfg and assign correct ownership and permissions (odroid:odroid, 755)

--- system-odroidgo2/firstboot2.sh
+++ system-odroidgo2/firstboot2.sh
@@ -6,5 +6,61 @@ mkdir -p /var/lib/alsa
 chmod 755 /var/lib/alsa
 amixer set 'Playback Path' 'SPK'
 alsactl store
-
+mkdir /roms/arcade/
+mkdir /roms/dreamcast/
+mkdir /roms/atomiswave/
+mkdir /roms/naomi/
+mkdir /roms/saturn/
+mkdir /roms/neogeo/
+mkdir /roms/cps1/
+mkdir /roms/cps2/
+mkdir /roms/cps3/
+mkdir /roms/cps/
+mkdir /roms/mame2003/
+mkdir /roms/mame/
+mkdir /roms/fds/
+mkdir /roms/nes/
+mkdir /roms/virtualboy/
+mkdir /roms/sufami/
+mkdir /roms/snes_msu1/
+mkdir /roms/snes/
+mkdir /roms/intellivision/
+mkdir /roms/n64/
+mkdir /roms/gb/
+mkdir /roms/gbc/
+mkdir /roms/gba/
+mkdir /roms/sgb/
+mkdir /roms/psx/
+mkdir /roms/nds/
+mkdir /roms/psp/
+mkdir /roms/sg-1000/
+mkdir /roms/mastersystem/
+mkdir /roms/megadrive/
+mkdir /roms/genesis/
+mkdir /roms/segacd/
+mkdir /roms/sega32x/
+mkdir /roms/gamegear/
+mkdir /roms/ngp/
+mkdir /roms/ngpc/
+mkdir /roms/tg16cd</path>
+mkdir /roms/tg16</path>
+mkdir /roms/pcengine/
+mkdir /roms/pcenginecd/
+mkdir /roms/supergrafx/
+mkdir /roms/vectrex/
+mkdir /roms/wonderswan/
+mkdir /roms/wonderswancolor/
+mkdir /roms/atari2600/
+mkdir /roms/atari5200/
+mkdir /roms/atari800/
+mkdir /roms/atari7800/
+mkdir /roms/atarilynx/
+mkdir /roms/msx2/
+mkdir /roms/msx/
+mkdir /roms/dos/
+mkdir /roms/coleco/
+mkdir /roms/scummvm/
+mkdir /roms/c64/
+chown -R odroid:odroid /roms
+chmod -R 755 /roms
 exit 0
