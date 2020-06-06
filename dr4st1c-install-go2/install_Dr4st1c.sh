6709e6e48dc7335b62415aee4ff94f64fcff12b0
npaladin2000 <43549566+npaladin2000@users.noreply.github.com>
2020-06-06 12:58:31 -0400

    Update install_Dr4st1c.sh
    New install script which detects platform and downloads appropriate cfg file

--- dr4st1c-install-go2/install_Dr4st1c.sh
+++ dr4st1c-install-go2/install_Dr4st1c.sh
@@ -1,6 +1,13 @@
 ##/bin/bash
-LINK="https://raw.githubusercontent.com/shantigilbert/binaries/master/odroid-xu4/drastic.tar.gz"
-LINKDEST="~/drastic.tar.gz"
-wget -O $LINKDEST $LINK
-tar xvf $LINKDEST 
-rm $LINKDEST
+cd ~
+wget -O ~/drastic.tar.gz https://raw.githubusercontent.com/shantigilbert/binaries/master/odroid-xu4/drastic.tar.gz
+tar xvf ~/drastic.tar.gz 
+rm ~/drastic.tar.gz
+mkdir ~/drastic/config/
+cd ~/drastic/config/
+if [ "cat /sys/devices/platform/odroidgo2-joypad/input/input2/name" = "odroidgo2_joypad_v11" ]; then
+    wget -O config.cfg https://raw.githubusercontent.com/valadaa48/retroroller/master/configs/OGA_V11/drastic.cfg
+else 
+    wget -O config.cfg https://raw.githubusercontent.com/valadaa48/retroroller/master/configs/OGA_V1/drastic.cfg
+fi
+wget -O config.cf2 https://raw.githubusercontent.com/valadaa48/retroroller/master/configs/OGA_V1/drastic.cf2
